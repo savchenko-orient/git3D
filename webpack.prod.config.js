ead99ed5501dd637ae16d0d82906e228e000454a
Oleksii Savchenko <savchenko.orient@gmail.com>
2021-01-30 11:53:40 +0200

    add babel

---
+++ webpack.prod.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    },
+                },
+                exclude: /node_modules/, 
+                // exclude: /(node_modules|src)/,
+            }
+        ]
+    }
+};
